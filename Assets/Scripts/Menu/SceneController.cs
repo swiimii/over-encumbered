76b87882486a178bb6e6b043bbdb980f307158ae
Samuel Scherer <swiimii962@gmail.com>
2020-12-21 18:25:06 -0500

    Start working on Menu stuff

---
+++ Assets/Scripts/Menu/SceneController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneController : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex) => SceneManager.LoadScene(sceneIndex);
+    public void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+}
