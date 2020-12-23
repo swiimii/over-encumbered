d1c4218b17d3d881d58c32d8f0ee94c62450b57b
Samuel Scherer <swiimii962@gmail.com>
2020-12-23 15:22:57 -0500

    Add briefcase counter

--- Assets/Scripts/Characters/Inventory.cs
+++ Assets/Scripts/Characters/Inventory.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     [SerializeField] int numOfBriefcases;
+    [SerializeField] int briefcasesNeeded;
+    [SerializeField] Text textBox;
     public List<Sprite> inventory;
-
+    private void Start() {
+        
+    }
     public void AddBriefcase(Sprite briefcase)
     {
         numOfBriefcases += 1;
         AddToInventory(briefcase);
+        DecrementBriefcaseTracker();
     }
     public void AddToInventory(Sprite item)
     {
         inventory.Add(item);
+    }
+    public void DecrementBriefcaseTracker()
+    {
+        textBox.text = (briefcasesNeeded-numOfBriefcases).ToString();
     }
 }
